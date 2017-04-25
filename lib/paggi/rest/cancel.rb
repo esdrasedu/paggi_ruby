236cebe6d3d1c003317ff5be2b3f37d561c4c8c8
Luis Daher <luisotaviodaher@gmail.com>
2017-04-24 23:39:26 -0300

    added remaining methods

---
+++ lib/paggi/rest/cancel.rb
@@ -0,0 +1,40 @@
+module Paggi
+  module Rest
+    module Cancel
+      class << self
+        def included(base)
+          base.extend(ClassMethods)
+        end
+      end
+
+      def cancel
+        result = self.class.cancel(to_json)
+        raise result if result.instance_of? StandardError
+
+        if result.instance_of? PaggiError
+          self.errors = result.errors
+          return false
+        end
+
+        initialize(result.to_json)
+        true
+      end
+
+      module ClassMethods
+        def cancel!(params = { id: 0 }, header = {})
+          cancel(params, header)
+        rescue PaggiError => e
+          build(params, e)
+        rescue StandardError => e
+          raise e
+        end
+
+        def cancel(params = { id: 0 }, header = {})
+          result = request("#{params[:id]}/cancel", params, :PUT, header)
+          raise result if result.is_a? StandardError
+          result
+        end
+      end
+    end
+  end
+end
